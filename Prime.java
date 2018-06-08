f3f3d8837d250bdde11f0c987932bfd23925a1e0
veneela <veneela83669@gmail.com>
2018-06-07 17:52:24 -0700

    Prime

---
+++ Prime.java
@@ -0,0 +1,15 @@
+class Prime {
+		public static void main(String args[]) {
+			for (int i = 2; i <= 1000; i++) {
+				for (int j = 2; j <= i; j++) {
+					if (j == i) {
+						System.out.println(i);
+					}
+					if (i % j == 0) {
+						break;
+					}
+				}
+			}
+		}
+	}
+
